5b127631dc217c042081b75521b4a8db320a71bc
SergeyKhmelik <sergey.khmelik@gmail.com>
2016-04-22 13:58:14 +0300

    changes

--- MoLb2/src/com/company/GradientDescentService.java
+++ MoLb2/src/com/company/GradientDescentService.java
@@ -14,7 +14,7 @@ public class GradientDescentService {
         List<IterationDto<Double>> iterations = new ArrayList<>();
 
         int k = 0;          //iteration counter
-        double a = 1d;      //step size
+        double a = 0.1d;     //step size
 
         double x1 = 1.5;
         double x2 = -2d;
@@ -24,7 +24,17 @@ public class GradientDescentService {
         IterationDto<Double> iterationData = new IterationDto(x1, x2, result);
         iterations.add(iterationData);
 
-        do{
+        for(int i = 0; i < 20; i++) {
+
+            System.out.println("Step " + (i + 1));
+            System.out.println("X1: " + x1 + ", X2: " + x2);
+            System.out.println("a:" + a);
+            System.out.println("Derivatives = " +
+                    "x1: " + x1Derivative.apply(x1, x2) +
+                    ", x2: " + x2Derivative.apply(x1, x2));
+
+            System.out.println("function result: " + function.apply(x1, x2));
+
             // 1. Change x1 & x2 by step
             double newX1, newX2;
             newX1 = x1 - a * x1Derivative.apply(x1, x2);
@@ -33,24 +43,55 @@ public class GradientDescentService {
             // 2. Find function result for new x1 & x2
             nextResult = function.apply(newX1, newX2);
 
+            System.out.println("RESULTS:\nold: " + function.apply(x1, x2) + " (x1: " + x1 + ", x2: " + x2 +")" + fullDerivative(x1Derivative, x1, x2Derivative, x2) +
+                    "\nnew: " + function.apply(newX1, newX2) + " (x1: " + newX1 + ", x2: " + newX2 +")" + fullDerivative(x1Derivative, newX1, x2Derivative, newX2));
+
             // 3. Update step if needed
             if (nextResult >= result) {
                 a = a / 2;
+            } else {
+                x1 = newX1;
+                x2 = newX2;
+                result = nextResult;
+                System.out.println("Result changed: full derivative:" +
+                        fullDerivative(x1Derivative, x1, x2Derivative, x2));
             }
 
-            x1 = newX1;
-            x2 = newX2;
-            result = nextResult;
+            System.out.println("-----------------------------------------------------------------------");
             //Add iteration data to result dto
             iterations.add(new IterationDto(x1, x2, result));
-        } while(fullDerivative(x1Derivative, x1, x2Derivative, x2) >= e);
+        }
 
-            return iterations;
+
+//        do{
+//            // 1. Change x1 & x2 by step
+//            double newX1, newX2;
+//            newX1 = x1 - a * x1Derivative.apply(x1, x2);
+//            newX2 = x2 - a * x2Derivative.apply(x1, x2);
+//
+//            // 2. Find function result for new x1 & x2
+//            nextResult = function.apply(newX1, newX2);
+//
+//            // 3. Update step if needed
+//            if (nextResult >= result) {
+//                a = a / 2;
+//            } else {
+//                x1 = newX1;
+//                x2 = newX2;
+//                result = nextResult;
+//            }
+//
+//            System.out.println("x1: " + x1 + "\tx2: " + x2 + "\tf: " + result + "\ta: " + a);
+//            //Add iteration data to result dto
+//            iterations.add(new IterationDto(x1, x2, result));
+//        } while(fullDerivative(x1Derivative, x1, x2Derivative, x2) >= e);
+
+        return iterations;
     }
 
     private static double fullDerivative(BiFunction<Double, Double, Double> x1Derivative, double x1,
                                          BiFunction<Double, Double, Double> x2Derivative, double x2) {
-        return x1Derivative.apply(x1, x2) + x2Derivative.apply(x1, x2);
+        return Math.sqrt(Math.pow(x1Derivative.apply(x1, x2), 2) + Math.pow(x2Derivative.apply(x1, x2),2));
     }
 
 }
--- MoLb2/src/com/company/Main.java
+++ MoLb2/src/com/company/Main.java
@@ -12,8 +12,8 @@ public class Main {
                 (x1, x2) -> Math.pow(x1, 3) + 2 * x1 * x2 + 6 * x1,
                 0.001);
 
-        results.forEach(iteration ->
-                System.out.println(iteration.getFirst() + " " + iteration.getSecond() + " " + iteration.getResult()));
+//        results.forEach(iteration ->
+//                System.out.println(iteration.getFirst() + " " + iteration.getSecond() + " " + iteration.getResult()));
 
 
         System.out.println(function(1.5, -3));
